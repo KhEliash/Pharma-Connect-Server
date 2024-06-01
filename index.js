27f782b789817e078e771c1b7db687d4d687a74a
Kh.Eliash <eliashebrahim@gmail.com>
2024-06-02 02:12:03 +0600

    connect mongodb and post userinfo to database

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 const express = require("express");
 const cors = require("cors");
-  require("dotenv").config();
- const app = express();
+require("dotenv").config();
+const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-    
-    ],
+    origin: ["http://localhost:5173"],
     credentials: true,
   })
 );
+app.use(express.json());
 
-
-
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xzzvi9v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,16 +22,28 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+
+    const userCollection = client.db("PharmaConnect").collection("users");
+
+    app.post('/users',async(req,res)=>{
+        const user = req.body;
+        const result = await userCollection.insertOne(user);
+         res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -45,16 +51,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Hello World! how");
+});
 
-
-
-
-app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.send('Hello World! how')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
