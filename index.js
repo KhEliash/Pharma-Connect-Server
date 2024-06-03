50a3cc4c220e419e0f0aa46a3e81058f19212bfd
Kh.Eliash <eliashebrahim@gmail.com>
2024-06-03 23:08:32 +0600

    prevent post existing user to database

--- index.js
+++ index.js
@@ -34,6 +34,12 @@ async function run() {
 
     app.post('/users',async(req,res)=>{
         const user = req.body;
+        console.log(user);
+        const query = {email:user.email}
+        const  existingUser = await userCollection.findOne(query);
+        if(existingUser){
+          return res.send("User already exists")
+        }
         const result = await userCollection.insertOne(user);
          res.send(result)
     })
